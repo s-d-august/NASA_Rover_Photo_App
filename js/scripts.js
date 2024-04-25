37caef8423e564a6c52a8c674badbed20b9a817d
s-d-august <sinclairaugust@gmail.com>
2024-04-24 19:46:39 -0500

    comments

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ let imgRepository = (function () {
     let imgList = [];
     let apiUrl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/' + roverChoice + '/latest_photos?api_key=DEMO_KEY'
 
-    function loadList() {
+    function loadList() { //loads image thumbnails and details and pushes them to the imgList array
         $.ajax(apiUrl, { dataType: 'json' }).then(function (responseJSON) {
             responseJSON.each(function (item) {
                 let thumbnail = {
@@ -15,5 +15,5 @@ let imgRepository = (function () {
         }).catch(function (e) {
             console.error(e);
         })
-    }
+    } // loadList
 })
