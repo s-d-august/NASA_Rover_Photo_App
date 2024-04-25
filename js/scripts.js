e69ee532680aae307594be4459c6bd54666bfe09
s-d-august <sinclairaugust@gmail.com>
2024-04-24 20:55:31 -0500

    fixed .each function

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,7 @@ let imgRepository = (function () {
 
     function loadList() { //loads image thumbnails and details and pushes them to the imgList array
         $.ajax(apiUrl, { dataType: 'json' }).then(function (responseJSON) {
-            responseJSON.each(function (item) {
+            $(responseJSON).each(function (item) {
                 let photo = {
                     img: item.img_src,
                     roverName: item.rover.name,
