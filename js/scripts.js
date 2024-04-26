64972bce8dba9990d9c7e27025690f14f5bf2353
s-d-august <sinclairaugust@gmail.com>
2024-04-26 12:42:50 -0500

    adjusted modal trigger to (hopefully) display correct contents

--- js/scripts.js
+++ js/scripts.js
@@ -38,20 +38,26 @@ let roverChoice = 'curiosity'
 
     function addThumbnail(photo) { //constructs array of thumbnails 
         let url = photo.img;
-        let id = photo.id;
+        let number = imgList.indexOf(photo);
         let thumbnailList = $('#thumbnails');
-        let thumbnailListItem = $(`<img class="col" data-toggle="modal" data-target="#exampleModal" data-whatever="` + id + `" src="` + url + `" style="width:150px;height:150px">`)
+        let thumbnailListItem = $(`<img class="col" data-toggle="modal" data-target="#exampleModal" data-whatever="` + number + `" src="` + url + `" style="width:150px;height:150px">`)
         thumbnailList.append(thumbnailListItem);
     }
 
 $('#exampleModal').on('show.bs.modal', function (event) {
         var button = $(event.relatedTarget) // Button that triggered the modal
-        var photo = button.data('whatever') // Extract info from data-* attributes
+        var number = button.data('whatever')
+        var contents = imgList[number] // Extract info from data-* attributes
         // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
         // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
         var modal = $(this)
-        modal.find('.modal-title').text('New message to ' + recipient)
-        modal.find('.modal-body input').val(recipient)
+        modal.find('.modal-title').text(contents.roverName + ', #' + contents.id)
+        modal.find('.modal-body img').attr('src', contents.img)
+        modal.find('.modal-body p').text(
+            `Camera: ` + contents.cameraName
+            `Martian Sol Date: ` + contents.solDate
+            `Earth Date: ` + contents.earthDate
+        )
       })
 
 loadList().then(function (){
