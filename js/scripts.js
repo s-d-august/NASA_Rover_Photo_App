b7bef722f0606d559ba32ec7fdde450f20e04fbe
s-d-august <sinclairaugust@gmail.com>
2024-06-10 16:50:08 -0500

    added comments, changed let to var

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,15 @@
 var photoArray = []
 
-//loads image thumbnails and details and pushes them to the imgList array
-
-let roverIndex = $(".active").attr("data-whatever");
+var roverIndex = $(".active").attr("data-whatever"); // gets the selected rover
 
 function addThumbnail(index, item) { //constructs grid of thumbnails
-  let thumbnailList = $('#thumbnails');
-  let thumbnailListItem = $(`<img class="col img-thumbnail" data-toggle="modal" data-target="#modal" 
+  var thumbnailList = $('#thumbnails');
+  var thumbnailListItem = $(`<img class="col img-thumbnail" data-toggle="modal" data-target="#modal" 
                 data-whatever="${index}" src="${item.img_src}">`) //data-whatever passes the index number so the details can be called in the modal
   thumbnailList.append(thumbnailListItem);
 }
 
+//loads image thumbnails and details and pushes them to photoArray
 Promise.all([
   fetch('https://mars-photos.herokuapp.com/api/v1/rovers/curiosity/latest_photos'),
   fetch('https://mars-photos.herokuapp.com/api/v1/rovers/opportunity/latest_photos'),
@@ -24,7 +23,7 @@ Promise.all([
       addThumbnail(index, item)
     })
 
-    $(".loading").addClass('d-none')
+    $(".load-text").addClass('d-none') // hides "Loading photos..." text
   })
 
   .catch((error) => console.error("An error occurred:", error));
@@ -33,12 +32,12 @@ Promise.all([
 //MODAL DISPLAY
 
 $('#modal').on('show.bs.modal', function (event) { // Triggers when modal is opened
-  let roverIndex = $(".active").attr("data-whatever");
+  var roverIndex = $(".active").attr("data-whatever");
   var thumbnail = $(event.relatedTarget) // Thumbnail that triggered the modal
   var number = thumbnail.data('whatever') // Extract info from data-* attributes
   var contents = photoArray[0][roverIndex].latest_photos[number] // Gets object from array using the index number variable from addThumbnail
 
-  let modalText = // template literal of modal text
+  var modalText = // template literal of modal text
     `Camera: ${contents.camera.full_name} (${contents.camera.name})
 Martian Sol Date: ${contents.sol}
 Earth Date: ${contents.earth_date}`
@@ -47,12 +46,12 @@ Earth Date: ${contents.earth_date}`
   modal.find('.modal-title').text(contents.rover.name + ', #' + contents.id)
   modal.find('.modal-body img').attr('src', contents.img_src)
   modal.find('.modal-body p').text(modalText)
-  let imgSize = (modal.find('img'))[0].naturalWidth // gets the size of the photo to adjust modal width
+  var imgSize = (modal.find('img'))[0].naturalWidth // gets the size of the photo to adjust modal width
   modalSize(imgSize, modal)
 })
 
 function modalSize(imgSize, modal) { //resizes the modal width based on photo size
-  let modalWidth = modal.find('.modal-dialog')
+  var modalWidth = modal.find('.modal-dialog')
 
   if (imgSize > 766) {
     modalWidth.attr('class', 'modal-dialog modal-xl')
@@ -63,20 +62,16 @@ function modalSize(imgSize, modal) { //resizes the modal width based on photo si
   else {
     modalWidth.attr('class', 'modal-dialog')
   }
-
 }
 
-
-
+// Clears thumbnails and generates new ones when rover selection changes
 $('.nav-link').on('click', function (event) {
-  $("#thumbnails").empty()
-  $('.nav-link').removeClass('active')
-  let selected = $(event.target);
-  let roverIndex = selected.data('whatever')
-  selected.addClass('active')
+  $("#thumbnails").empty() // Clears thumbnails
+  $('.nav-link').removeClass('active') // Removes .active from all rovers
+  var selected = $(event.target);
+  var roverIndex = selected.data('whatever') // Index of selected rover in photoArray
+  selected.addClass('active') // Adds .active to selected rover
   $.each((photoArray[0][roverIndex].latest_photos), function (index, item) {
     addThumbnail(index, item)
   })
-
-
-}) // loadList function
+})
